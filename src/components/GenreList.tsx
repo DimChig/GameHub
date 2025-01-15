d97e6dcfbca1daebbf60fa4afbae5d9a19ac00f8
DimChig <dima.chigarev@gmail.com>
2025-01-15 00:54:17 -0500

    genre skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,42 @@
 import useGenres from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) {
+    let skeletons_count = 20;
+    let skeletons = [];
+    for (let i = 0; i < skeletons_count; i++) skeletons.push(i);
+
+    return (
+      <List>
+        {skeletons.map((skeleton_id) => (
+          <ListItem key={skeleton_id} paddingY={"5px"}>
+            <HStack width="full">
+              <Skeleton
+                boxSize={"32px"}
+                borderRadius={"8px"}
+                objectFit="cover"
+              ></Skeleton>
+              <SkeletonText noOfLines={2} flex={1}></SkeletonText>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
 
   return (
     <List>
@@ -14,6 +47,7 @@ const GenreList = () => {
               boxSize={"32px"}
               borderRadius={"8px"}
               src={getCroppedImageUrl(genre.image_background)}
+              objectFit="cover"
             ></Image>
             <Text fontSize="lg">{genre.name}</Text>
           </HStack>
