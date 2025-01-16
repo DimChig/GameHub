e6efebbbe271e8d8ad92e631c4ed2ffc12fe1981
DimChig <dima.chigarev@gmail.com>
2025-01-15 20:48:41 -0500

    added search bar

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -30,7 +31,11 @@ function App() {
         }}
       >
         <GridItem area="nav" bg="_coral">
-          <NavBar />
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="_blue" paddingX={"5px"}>
