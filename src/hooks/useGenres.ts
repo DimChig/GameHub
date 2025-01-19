55cfffaebe0334c600249a84e785bc3193f34dbe
DimChig <dima.chigarev@gmail.com>
2025-01-19 17:23:45 -0500

    simplify time calculations with ms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "@/services/api-client";
+import APIClient, { FetchResponse } from "@/services/api-client";
 import genres from "@/data/genres";
-
+import ms from "ms";
 const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
@@ -12,10 +12,10 @@ export interface Genre {
 
 //const useGenres = () => useData<Genre>("/genres");
 const useGenres = () =>
-  useQuery({
+  useQuery<FetchResponse<Genre>>({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms("24h"),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
 import platforms from "@/data/platforms";
 import APIClient from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -10,13 +11,13 @@ export interface Platform {
   slug: string;
 }
 
-//const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 // const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 
