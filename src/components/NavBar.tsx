a551b97d584c420807fbc7324f902526872a2104
DimChig <dima.chigarev@gmail.com>
2025-01-22 12:58:52 -0500

    fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,20 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px" paddingRight="20px" marginTop={"10px"}>
-      <Image src={logo} boxSize="40px" borderRadius={10}></Image>
+      <Link to={"/"}>
+        <Image
+          src={logo}
+          boxSize="40px"
+          objectFit={"cover"}
+          minWidth={"40px"}
+          borderRadius={10}
+        ></Image>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
