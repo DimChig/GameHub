97bd61e6665b2543022e75778f796318e81180dc
DimChig <dima.chigarev@gmail.com>
2025-01-21 22:16:27 -0500

    setup routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import GameGrid from "@/components/GameGrid";
+import GameHeading from "@/components/GameHeading";
+import GenreList from "@/components/GenreList";
+import PlatformSelector from "@/components/PlatformSelector";
+import SortSelector from "@/components/SortSelector";
+import { Grid, Show, GridItem, Box, HStack } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" bg="_blue" paddingX={"10px"}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="_dodgerblue">
+        <Box marginLeft={"10px"}>
+          <GameHeading />
+          <HStack spacing={3} marginBottom={3}>
+            <PlatformSelector />
+            <SortSelector />
+          </HStack>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
